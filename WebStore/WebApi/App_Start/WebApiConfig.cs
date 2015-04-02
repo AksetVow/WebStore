ae3435d209288059909d5c6cd140147a6995b0ba
Roman_Slyvka <roman_slyvka@epam.com>
2015-04-02 17:03:52 +0200

    Change default format of WebApi response

--- WebStore/WebApi/App_Start/WebApiConfig.cs
+++ WebStore/WebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebApi
@@ -11,6 +12,8 @@ namespace WebApi
         {
             config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
