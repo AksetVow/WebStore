d0257dba5390614eda44d984ac4dc4ad40cddc04
Roman Slyvka <rslyvka@gmail.com>
2015-08-27 00:56:45 +0300

    Create request form

---
+++ GSPA/app/scripts/controllers/application.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gspaApp.controller:ApplicationCtrl
+ * @description
+ * # ApplicationCtrl
+ * Controller of the gspaApp
+ */
+angular.module('gspaApp')
+  .controller('ApplicationCtrl', function ($scope) {
+  });
